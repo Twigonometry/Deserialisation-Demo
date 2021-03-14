db979274570a45bbf7a279e4eb6aa6d481d76060
Twigonometry <mac.j.goodwin@gmail.com>
2021-03-14 19:17:54 +0000

    Basic unsafe program

---
+++ unsafe.php
@@ -0,0 +1,34 @@
+<?php
+
+class SignupForm
+{
+    public $outfile = "signups.txt";
+    public $username_string = "";
+    
+    //parse their username
+    public function parse_username($username)
+    {
+    	echo "Parsing username...<br>";
+    	$this->username_string = "Username: " . $username;
+    	
+    	//TODO: remove swear words from the username >:(
+    }
+
+    //do this after everything else is finished
+    public function __destruct()
+    {
+    	//write the username to file
+    	file_put_contents(__DIR__ . '/' . $this->outfile, $this->username_string);
+    	echo "Added your username to the signups file!<br>";
+    }
+}
+
+//get their username from the request parameter
+$username_input = $_GET['username'];
+
+//unpack the data so we can use it later!
+//TODO: check this is safe - I think I saw a Medium article somewhere
+$unserialised = unserialize($username_input);
+
+$signup_handler = new SignupForm;
+$signup_handler->parse_username($username_input);
